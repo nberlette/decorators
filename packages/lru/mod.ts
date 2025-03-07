a6589731fb6f9080027d34f5afb562a16d0412cd
Nicholas Berlette <nick@berlette.com>
2025-03-07 09:34:11 +0000

    fix(lru): add missing exports to root module

--- packages/lru/mod.ts
+++ packages/lru/mod.ts
@@ -3,6 +3,8 @@
  */
 export * from "./src/lru_cache.ts";
 export * from "./src/lru_decorator.ts";
+export * from "./src/overrides.ts";
+export * from "./src/options.ts";
 export * from "./src/types.ts";
 
 export { default } from "./src/lru_decorator.ts";
